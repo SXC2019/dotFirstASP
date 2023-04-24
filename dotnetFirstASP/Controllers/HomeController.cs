5afc7159eac47236549937e454a9eb306052cb63
Rojan Thapa <rojaneverest@gmail.com>
2023-04-24 08:33:18 +0545

    first commit

---
+++ dotnetFirstASP/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication5.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult AboutUs()
+        {
+            return View("About");
+        }
+
+        public IActionResult Add()
+        {
+            int a = 3, b = 0, c;
+            c = a / b;
+
+            string fname = Request.Query["txtFirst"];
+            string lname = Request.Query["txtSecond"];
+
+            string fullName = fname + lname;
+
+            ViewBag.Info= fullName; 
+            return View("Information");
+        }
+
+        public IActionResult Error()
+        {
+
+            return View("PageNotFound");
+        }
+    }
+}
---
+++ dotnetFirstASP/Controllers/StudentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication5.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
