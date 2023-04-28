e7bf68774dd1676a06c6cda4c20ebfe12800fecc
SXC2019 <labwork70@gmail.com>
2023-04-28 08:58:40 +0545

    database included

--- WebApplication5/Controllers/HomeController.cs
+++ WebApplication5/Controllers/HomeController.cs
@@ -1,13 +1,60 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using WebApplication5.Models;
 
 namespace WebApplication5.Controllers
 {
   
     public class HomeController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
-        {
+        {        
+
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(ModelStuudent student)
+        {
+
+           
+            string connectionStri = "Data Source=DESKTOP-I446C2N;uid=sa;pwd=sql;Initial Catalog=Test";
+            SqlConnection con = new SqlConnection(connectionStri);
+            string sql = "insert into tblStudent values(@Roll,@FirstName)";
+            //string sql = "delete from tblStudent where Roll=@Roll";
+            //string sql = "update tblStudent set FirstName=@FirstName where Roll=@Roll";
+            SqlCommand command = new SqlCommand(sql, con);
+            command.Parameters.AddWithValue("@Roll", student.Roll);
+            command.Parameters.AddWithValue("@FirstName", student.FirstName);
+
+            con.Open();
+            command.ExecuteNonQuery();
+            con.Close();
+            ViewBag.Info = "Data Saved Successfully";
+            return View("Information");
+        }
+
+        [HttpPost]
+        public  IActionResult Add()
+        {
+            int roll = Convert.ToInt32( Request.Form["txtRoll"]);
+            string name = Request.Form["txtFirstName"];
+            string connectionStri = "Data Source=DESKTOP-I446C2N;uid=sa;pwd=sql;Initial Catalog=Test";
+            SqlConnection con = new SqlConnection(connectionStri);
+            //string sql = "insert into tblStudent values(@Roll,@FirstName)";
+            //string sql = "delete from tblStudent where Roll=@Roll";
+            string sql = "update tblStudent set FirstName=@FirstName where Roll=@Roll";
+            SqlCommand command = new SqlCommand(sql, con);
+            command.Parameters.AddWithValue("@Roll", roll);
+           command.Parameters.AddWithValue("@FirstName", name);
+
+            con.Open();
+            command.ExecuteNonQuery();
+            con.Close();
+            ViewBag.Info= "Data Saved Successfully";
+            return View("Information");
+
         }
 
         [Route("Mero/con")]
@@ -22,7 +69,8 @@ namespace WebApplication5.Controllers
             return View("About");
         }
 
-        public IActionResult Add()
+        [HttpGet]
+        public IActionResult Add(int a)
         {
             
             
---
+++ WebApplication5/Models/ModelStuudent.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication5.Models
+{
+    public class ModelStuudent
+    {
+        public int Roll { get; set; }
+        public string FirstName { get; set; }
+    }
+}
